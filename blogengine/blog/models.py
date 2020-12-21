22dbb9e008adbfeddd5957b66c1bb93c30380685
Mikhail Knyazev <knyazevmo@gmail.com>
2020-12-21 21:17:33 +0300

    миксины и начало работы с формами

--- blogengine/blog/models.py
+++ blogengine/blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
 
 
     def __str__(self):
-        return f'{self.title}'
+        return self.title
 
 
 class Tag(models.Model):
--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from .models import Post, Tag
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import View
+from .models import *
+from .utils import ObjectDetailMixin
+from .forms import TagForm
 
 
 def posts_list(request):
@@ -7,9 +10,16 @@ def posts_list(request):
     return render(request, 'blog/index.html', context={'posts': posts})
 
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+    
+
+class TagCreate(View):
+    def get(self, request):
+        form = TagForm()
+        return render(request, 'blog/tag_create.html', context={'form': form})
+
 
 
 def tags_list(request):
@@ -17,6 +27,6 @@ def tags_list(request):
     return render(request, 'blog/tags_list.html', context={'tags': tags})
 
 
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'

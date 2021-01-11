560b2ad13ed1315ca7666f014d2ca2df2039e7ec
Mikhail Knyazev <knyazevmo@gmail.com>
2021-01-11 18:52:08 +0300

    доделал пагинацию страниц и сделал нормальное отображение + next and pervious

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -10,10 +10,27 @@ from django.core.paginator import Paginator
 
 def posts_list(request):
     posts = Post.objects.all()
-    paginator = Paginator(posts, 2)
+    paginator = Paginator(posts, 3)
     page_number = request.GET.get('page', 1) # если не будет найдено, то будет подставлена 1
     page = paginator.get_page(page_number)
-    return render(request, 'blog/index.html', context={'posts': page})
+    is_paginated = page.has_other_pages()
+
+    if page.has_previous() == True:
+        prev_url = f'?page={page.previous_page_number()}'
+    else:
+        prev_url = ''
+    if page.has_next() == True:
+        next_url = f'?page={page.next_page_number()}'
+    else:
+        next_url = ''
+    
+    context={
+        'page_object': page,
+        'is_paginated': is_paginated,
+        'prev_url': prev_url,
+        'next_url': next_url,
+    }
+    return render(request, 'blog/index.html', context=context)
 
 
 class PostDetail(ObjectDetailMixin, View):
